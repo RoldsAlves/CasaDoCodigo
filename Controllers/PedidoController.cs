59655074177d2abd9399ee13d1fa04fd4b18121e
Rodolfo Alves <rodolfo.s.a@hotmail.com>
2021-06-18 21:34:45 -0300

    Criando o Projeto - Página Carrossel

---
+++ Controllers/PedidoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoAspNetCore.Controllers
+{
+    public class PedidoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Carrossel()
+        {
+
+            return View();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -26,15 +26,16 @@ namespace CursoAspNetCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddApplicationInsightsTelemetry();
             //services.AddTransient<ICatalogo, Catalogo>();
             //services.AddTransient<IRelatorio, Relatorio>();
 
             //services.AddScoped<ICatalogo, Catalogo>();
             //services.AddScoped<IRelatorio, Relatorio>();
 
-            var catalogo = new Catalogo();
-            services.AddSingleton<ICatalogo>(catalogo);
-            services.AddSingleton<IRelatorio>(new Relatorio(catalogo));
+            //var catalogo = new Catalogo();
+            //services.AddSingleton<ICatalogo>(catalogo);
+            //services.AddSingleton<IRelatorio>(new Relatorio(catalogo));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,25 +51,25 @@ namespace CursoAspNetCore
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            ICatalogo catalogo = serviceProvider.GetService<ICatalogo>();
-            IRelatorio relatorio = serviceProvider.GetService<IRelatorio>();
-            app.Run(async (context) =>
-            {
-                await relatorio.Imprimir(context);
-            });
-            //app.UseHttpsRedirection();
-            //app.UseStaticFiles();
-
-            //app.UseRouting();
-
-            //app.UseAuthorization();
-
-            //app.UseEndpoints(endpoints =>
+            //ICatalogo catalogo = serviceProvider.GetService<ICatalogo>();
+            //IRelatorio relatorio = serviceProvider.GetService<IRelatorio>();
+            //app.Run(async (context) =>
             //{
-            //    endpoints.MapControllerRoute(
-            //        name: "default",
-            //        pattern: "{controller=Home}/{action=Index}/{id?}");
+            //    await relatorio.Imprimir(context);
             //});
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
 
         }
     }
