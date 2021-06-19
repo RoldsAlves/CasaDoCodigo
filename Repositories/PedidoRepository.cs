2b8f0f45a62349c55430de281a0acc2d09dfd71c
Rodolfo Alves <rodolfo.s.a@hotmail.com>
2021-06-19 19:09:01 -0300

    Carrinho e Resumo - Salvando item de pedido

--- Repositories/PedidoRepository.cs
+++ Repositories/PedidoRepository.cs
@@ -20,6 +20,28 @@ namespace CursoAspNetCore.Repositories
             this.contextAcessor = contextAccessor;
         }
 
+        public void AddItem(string codigo)
+        {
+            var produto = context.Set<Produto>().Where(p => p.Codigo == codigo).SingleOrDefault();
+
+            if(produto == null)
+            {
+                throw new ArgumentException("Produto Não Encontrado!");
+            }
+
+            var pedido = GetPedido();
+
+            var itemPedido = context.Set<ItemPedido>()
+                .Where(i => i.Produto.Codigo == codigo && i.Pedido.Id == pedido.Id)
+                .SingleOrDefault();
+            if (itemPedido == null)
+            {
+                itemPedido = new ItemPedido(pedido, produto, 1, produto.Preco);
+                context.Set<ItemPedido>().Add(itemPedido);
+                context.SaveChanges();
+            }
+        }
+
         public Pedido GetPedido()
         {
             var pedidoId = GetPedidoId();
