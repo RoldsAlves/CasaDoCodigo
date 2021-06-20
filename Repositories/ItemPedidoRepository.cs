25f75c1361146b24c8cec7f3228da4cb1ab2fa14
Rodolfo Alves <rodolfo.s.a@hotmail.com>
2021-06-20 14:05:48 -0300

    Alterando a quantidade de itens - Alterando a quantidade no banco de dados

--- Repositories/ItemPedidoRepository.cs
+++ Repositories/ItemPedidoRepository.cs
@@ -8,12 +8,23 @@ namespace CasaDoCodigo.Repositories
 {
     public interface IItemPedidoRepository
     {
-
+        void UpdateQuantidade(ItemPedido itemPedido);
     }
     public class ItemPedidoRepository : BaseRepository<ItemPedido>, IItemPedidoRepository
     {
         public ItemPedidoRepository(ApplicationContext context) : base(context)
         {
+        }
+
+        public void UpdateQuantidade(ItemPedido itemPedido)
+        {
+            var itemPedidoDb =  dbSet.Where(ip => ip.Id == itemPedido.Id).SingleOrDefault();
+
+            if(itemPedidoDb != null)
+            {
+                itemPedidoDb.AtualizaQuantidade(itemPedido.Quantidade);
+                context.SaveChanges();
+            }
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -22,6 +22,7 @@ namespace CasaDoCodigo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews().AddNewtonsoftJson();
             services.AddControllersWithViews();
             services.AddApplicationInsightsTelemetry();
             services.AddMvc();
