e2d14acb1c238dc2784309a33aa27cb6738c675c
Rodolfo Alves <rodolfo.s.a@hotmail.com>
2021-06-24 10:55:31 -0300

    ASP.NET Core 3 - Integrando a LoginPartial no Front End

--- Startup.cs
+++ Startup.cs
@@ -98,6 +98,7 @@ namespace CasaDoCodigo
             }
 
             app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseSession();
             app.UseMvc(routes =>
             {
