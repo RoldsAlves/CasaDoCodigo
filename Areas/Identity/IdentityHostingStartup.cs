ba0b277e5974ca9951a4ffad8bd620b7fdc42f12
Rodolfo Alves <rodolfo.s.a@hotmail.com>
2021-06-24 11:34:50 -0300

    ASP.NET Core 3 - RazorPages - Implementando a Classe Descritora de Erros

--- Areas/Identity/IdentityHostingStartup.cs
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -21,8 +21,153 @@ namespace CasaDoCodigo.Areas.Identity
                         context.Configuration.GetConnectionString("AppIdentityContextConnection")));
 
                 services.AddDefaultIdentity<AppIdentityUser>()
+                    .AddErrorDescriber<IdentityErrorDescriberPtBr>()
                     .AddEntityFrameworkStores<AppIdentityContext>();
             });
+        }
+    }
+
+    internal class IdentityErrorDescriberPtBr : IdentityErrorDescriber
+    {
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public override IdentityError DefaultError()
+        {
+            return base.DefaultError();
+        }
+
+        public override IdentityError ConcurrencyFailure()
+        {
+            return base.ConcurrencyFailure();
+        }
+
+        public override IdentityError PasswordMismatch()
+        {
+            return base.PasswordMismatch();
+        }
+
+        public override IdentityError InvalidToken()
+        {
+            return base.InvalidToken();
+        }
+
+        public override IdentityError RecoveryCodeRedemptionFailed()
+        {
+            return base.RecoveryCodeRedemptionFailed();
+        }
+
+        public override IdentityError LoginAlreadyAssociated()
+        {
+            return base.LoginAlreadyAssociated();
+        }
+
+        public override IdentityError InvalidUserName(string userName)
+        {
+            return base.InvalidUserName(userName);
+        }
+
+        public override IdentityError InvalidEmail(string email)
+        {
+            return base.InvalidEmail(email);
+        }
+
+        public override IdentityError DuplicateUserName(string userName)
+        {
+            return base.DuplicateUserName(userName);
+        }
+
+        public override IdentityError DuplicateEmail(string email)
+        {
+            return base.DuplicateEmail(email);
+        }
+
+        public override IdentityError InvalidRoleName(string role)
+        {
+            return base.InvalidRoleName(role);
+        }
+
+        public override IdentityError DuplicateRoleName(string role)
+        {
+            return base.DuplicateRoleName(role);
+        }
+
+        public override IdentityError UserAlreadyHasPassword()
+        {
+            return base.UserAlreadyHasPassword();
+        }
+
+        public override IdentityError UserLockoutNotEnabled()
+        {
+            return base.UserLockoutNotEnabled();
+        }
+
+        public override IdentityError UserAlreadyInRole(string role)
+        {
+            return base.UserAlreadyInRole(role);
+        }
+
+        public override IdentityError UserNotInRole(string role)
+        {
+            return base.UserNotInRole(role);
+        }
+
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return base.PasswordTooShort(length);
+        }
+
+        public override IdentityError PasswordRequiresUniqueChars(int uniqueChars)
+        {
+            return base.PasswordRequiresUniqueChars(uniqueChars);
+        }
+
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresNonAlphanumeric),
+                Description = "A senha deve ter pelo menos um caractere alfanumérico."
+            };
+        }
+
+        public override IdentityError PasswordRequiresDigit()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresDigit),
+                Description = "A senha deve ter pelo menos um caractere numérico (0 - 9)."
+            };
+        }
+
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresLower),
+                Description = "A senha deve ter pelo menos uma letra minúscula ('a' - 'z')."
+            };
+        }
+
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresUpper),
+                Description = "A senha deve ter pelo menos uma letra maiúscula ('A' - 'Z')."
+            };
         }
     }
 }
